df7269b17db296d103afd4e5edcebdacca050ae0
Tomentu <43002796+Tomentu@users.noreply.github.com>
2019-11-03 12:51:38 +0800

    Create boxMoveWithMouse.js

---
+++ boxMoveWithMouse.js
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>hello word</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+    <link rel="stylesheet" href="base.css">
+    <style>
+        #div-parent{
+            position: relative;
+            background: black;
+            height: 200px;
+            width: 200px;
+        }
+        #div-child{
+            position: absolute;
+            background: red;
+            height: 50px;
+            width: 50px;
+            top:10px;
+            left: 0px;
+        }
+    </style>
+</head>
+<body>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        
+    </ul>
+    <div id="div-parent">
+        <div id="div-child">
+
+        </div>
+    </div>
+    <script>
+        window.onload=function(e){
+            const child  = document.getElementById("div-child");
+            child.onmousedown=function(e){
+                var x = e.clientX;
+                var y = e.clientY;
+                //鼠标相对与box的位置
+                var relatvieMouseY = child.offsetTop-y;
+                var relativeMouseX = child.offsetLeft-x;
+                //最大的相对位置
+                var maxPositionY = child.offsetParent.clientHeight-child.clientHeight;
+                var maxPositionX = child.offsetParent.clientWidth-child.clientWidth;
+                document.onmousemove=function(e){
+                   var newX = e.clientX;
+                   var newY = e.clientY;
+                   var positionX = newX+relativeMouseX;
+                   var positionY = newY+relatvieMouseY;
+                   positionX = positionX<0?0:positionX;
+                   positionX = positionX>maxPositionX?maxPositionX:positionX;
+                   positionY = positionY<0?0:positionY;
+                   positionY = positionY>maxPositionY?maxPositionY:positionY;
+                   child.style.top = positionY+"px";
+                   child.style.left = positionX+"px"; 
+                }
+            }
+            document.onmouseup=function(){
+                document.onmousemove = null;
+            }
+        }
+    </script>
+</body>
+</html>
